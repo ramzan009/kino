e3258e9ba1d8324f5d76e8e790673ef00c02bc1e
Дикаев Рахман Расулович <rahman.corporative@gmail.com>
2025-02-24 11:57:33 +0300

    Фикс замечания
    1. Исправил ошибку

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -14,7 +13,6 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-    use SoftDeletes;
     use CrudTrait;
 
     /**
